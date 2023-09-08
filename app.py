988550ced6cd8f0862ec2324e056c35d7847fec2
SholehSarir <136331640+SholehSarir@users.noreply.github.com>
2023-09-09 00:39:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-
 import pandas as pd
 import streamlit as st
-import numpy as np
-import pickle
+from absenteeism_module import absenteeism_model
 import warnings
+
+# Disable warnings for cleaner output
 warnings.filterwarnings("ignore")
 
+def input_output():
+    """
+    Streamlit app for absenteeism prediction.
 
-from absenteeism_module import *
+    Collects user input via file upload, and makes predictions based on the uploaded data.
+    """
+    st.title("Absenteeism Prediction")
 
-
-def Input_Output():
-    data = st.file_uploader("Please Upload Your File Here", type={"csv", "txt"})
+    data = st.file_uploader("Please Upload Your File Here", type=["csv", "txt"])
+    
     if data is not None:
         df = pd.read_csv(data)
         st.write(df)
+        
         model = absenteeism_model('model', 'scaler')
-
         model.load_and_clean_data('Absenteeism_new_data.csv')
 
-    result = ""
-    if st.button("Click here to Predict"):
-        result = model.predicted_outputs()
-        st.balloons()     
-    st.success('The output is as follows: ')
-    st.write(result)
+        result = ""
+        
+        if st.button("Click here to Predict"):
+            result = model.predicted_outputs()
+            st.balloons() 
 
+        st.success('The output is as follows: ')
+        st.write(result)
 
-if __name__ ==  '__main__':
-    Input_Output()
+if __name__ == '__main__':
+    input_output()
